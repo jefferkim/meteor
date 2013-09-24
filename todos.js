9ab3d1d46354a36cc6bf635d284ec0fdd39cb384
驭剑 <jianfeng.jjf@taobao.com>
2013-09-24 10:54:13 +0800

    edit

--- todos.js
+++ todos.js
@@ -6,6 +6,12 @@ if (Meteor.isClient) {
   Template.todos.todoList = function() {
     return Todos.find({}, {sort: {time: -1}});
   };
+  Template.count.completedCount = function(){
+    return Todos.find({done:1}).count();
+  };
+  Template.count.undoneCount = function(){
+    return Todos.find({done:0}).count();
+  }
 
 
   Template.todos.events({
@@ -14,12 +20,24 @@ if (Meteor.isClient) {
       var deleteBtn = target.children[2];
       deleteBtn.style.display = 'block';
     },
+
     'click .destroy': function(e) {
       e.preventDefault();
-      var todoId = e.currentTarget.getAttribute('data-id');
+      var todoId = e.currentTarget.parentNode.getAttribute('data-id');
       Todos.remove(todoId);
+    },
+    
+    'click .toggle':function(e){
+      var target = e.currentTarget;
+      var todoId = target.parentNode.getAttribute('data-id');
+      
+      Todos.update(todoId,{$set: {done:target.checked ? 1 :0}});
+
+
     }
   });
+
+
 
   Template.addTodo.events({
     'keydown': function() {
@@ -28,7 +46,8 @@ if (Meteor.isClient) {
         if (input.value && input.value != '') {
           Todos.insert({
             title: input.value,
-            time: Date.now()
+            time: Date.now(),
+            done: 0
           });
           input.value = '';
         }
